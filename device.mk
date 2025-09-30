0e957c981258ce8034053bee22235768319e4680
AmikuraYuki <amikura.yuki@gmail.com>
2025-09-30 17:43:37 +0700

    fog: update tree
    Signed-off-by: AmikuraYuki <amikura.yuki@gmail.com>

--- device.mk
+++ device.mk
@@ -31,17 +31,20 @@ PLATFORM_VERSION := 16.1.0
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    vendor.display.config@1.0 \
-    vendor.display.config@2.0 \
-    vendor.qti.hardware.vibrator.service \
-    vendor.qti.hardware.vibrator.impl \
-    libqtivibratoreffect
-
 #Properties
 #TW_OVERRIDE_SYSTEM_PROPS := \
 #    "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
 
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+
+RECOVERY_BINARY_SOURCE_FILES += \
+    $(TARGET_OUT_VENDOR_EXECUTABLES)/hw/vendor.qti.hardware.vibrator.service
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.qti.hardware.vibrator.impl.so \
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libqtivibratoreffect.so
