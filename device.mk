71340c1a0e201304b6638e0927651ae2d9eed307
AmikuraYuki <amikura.yuki@gmail.com>
2025-10-02 23:53:00 +0700

    fog: update tree
    Signed-off-by: AmikuraYuki <amikura.yuki@gmail.com>

--- device.mk
+++ device.mk
@@ -28,6 +28,28 @@ AB_OTA_PARTITIONS += \
     vendor \
     vendor_boot
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    checkpoint_gc \
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
